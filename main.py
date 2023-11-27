0b220e2472c271b256c8c888f5a6cfc7f5851d9c
Marcin <marcin.karbowniczyn@gmail.com>
2023-11-27 17:37:27 +0100

    Send an email with news and titles and descriptions

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import requests
-import dotenv
 import os
+from dotenv import load_dotenv
+from send_email import send_email
 
-dotenv.load_dotenv()
+load_dotenv()
+
 api_key = os.getenv('API_KEY')
+topic = 'tesla'
 
-url = f"https://newsapi.org/v2/everything?q=tesla&from=2023-10-27&sortBy=publishedAt&apiKey={api_key}"
+url = f"https://newsapi.org/v2/everything?q={topic}&from=2023-10-27&sortBy=publishedAt&apiKey={api_key}"
 
 # Make a request
-request = requests.get('https://acmilan.com.pl')  # This method creates "request object type"
+request = requests.get(url)  # This method creates "request object type"
 
 # Get a dictionary with a data
 content = request.json()  # This method converts the response into dict type. This will throw an error if response cannot be converted into dict from json.
 
-# Access article titles and descriptions
+# Access article titles and descriptions and construct a email content out of it
+message = ''
 for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
+    if article['title'] is not None:
+        message = message + article['title'] + '\n' + article['description'] + '\n' + '\n'
+
+    # message = message + f"""
+    # {article['title']}
+    # {article['description']}
+    # """
+
+message = message.encode('utf-8')
+send_email(message)
---
+++ send_email.py
@@ -0,0 +1,25 @@
+import smtplib  # Standard lib to send emails. Lecture 218
+import ssl
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def send_email(message):
+    host = os.getenv('HOST')
+    port = os.getenv('PORT')
+    username = os.getenv('USERNAME_GMAIL')
+    password = os.getenv('PASSWORD_GMAIL')
+
+    reciever = 'marcin.karbowniczyn@gmail.com'
+    message_to_send = f"""\
+Subject: Daily News Digest
+
+{message}
+"""
+
+    context = ssl.create_default_context()  # Nie wiem co to, mogę później poszukać
+    with smtplib.SMTP_SSL(host, port, context=context) as server:  # Musimy dać "as" bo otwieramy funkcję która zwraca zmienną na której są metody
+        server.login(username, password)
+        server.sendmail(username, reciever, message_to_send)
